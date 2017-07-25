321b7fe12a0c2714a37e7063d6698aa59ad92874
MBaran88 <zdzibq@gmail.com>
2017-07-25 19:15:53 +0200

    added DatabaseClientsDAO

--- src/main/java/DatabaseClientsDAO.java
+++ src/main/java/DatabaseClientsDAO.java
@@ -3,5 +3,30 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-public class DatabaseClientsDAO {
+
+public class DatabaseClientsDAO implements IDAO<User> {
+    private static DatabaseClientsDAO instance = null;
+
+    public static void init(DatabaseServer server){
+        if (instance == null){
+            instance = new DatabaseClientsDAO(server);
+        }
+    }
+    public static DatabaseClientsDAO getInstance(){
+        return instance;
+    }
+    public static final String CLIENT_ID_COLUMN = "client_id";
+    public static final String CLIENT_NAME_COLUMN = "client_name";
+    public static final String CLIENT_SURNAME_COLUMN = "client_surname";
+    public static final String EMPLOYEE_ID_COLUMN = "employee_id";
+    public static final String EMPLOYEE_NAME_COLUMN = "employee_name";
+    public static final String EMPLOYEE_SURNAME_COLUMN = "employee_surname";
+
+
+
+    public static final String GET_ALL_SQL ="select clients.name,clients.surname,employees.name,employees.surname from clients inner join employees where employees.surname = 'Hoss'";
+
+    private DatabaseServer server;
+
+    private DatabaseClientsDAO
 }
--- src/main/java/IDAO.java
+++ src/main/java/IDAO.java
@@ -1,3 +1,8 @@
-public interface IDAO extends Bean> {
-    List
+import java.util.List;
+
+public interface IDAO<T extends Bean> {
+    List<T> get();
+    void add(T beam);
+    void update(T beam);
+    void delete (T beam);
 }
